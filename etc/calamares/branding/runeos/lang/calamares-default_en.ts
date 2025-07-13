bd26be399421ad35c67e7f3b5c96cd27b83289cf
behrouziyeh <204469443+behrouziyeh@users.noreply.github.com>
2025-07-13 17:48:42 +0330

    added basic calamares configs

---
+++ etc/calamares/branding/runeos/lang/calamares-default_en.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>show</name>
+    <message>
+        <location filename="../show.qml" line="43"/>
+        <source>RuneOS is an Arch-based distro.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../show.qml" line="51"/>
+        <source>It has it\'s declarative pacman wrapper, Rune.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../show.qml" line="55"/>
+        <source>By it\'s arch-based nature, It\'s a Rolling release distro.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
